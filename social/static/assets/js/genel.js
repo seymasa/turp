c108b46b2eb6b14684bc9d0438cadb3d9ad4c6c9
Şeyma Sarıgil <seymas@kariyer.net>
2016-08-02 17:17:18 +0300

    l

--- social/static/assets/js/genel.js
+++ social/static/assets/js/genel.js
@@ -21,22 +21,3 @@ $(document).on('click', '.button-like', function () {
    console.log(id);
    return false;
 });
-/*
-
-<script>
-$('#like').click(function(){
-      $.ajax({
-               type: "POST",
-               url: "{% url 'like' %}",
-               data: {'slug': $(this).attr('name'), 'csrfmiddlewaretoken': '{{ csrf_token }}'},
-               dataType: "json",
-               success: function(response) {
-                      alert(response.message);
-                      alert('Company likes count is now ' + response.likes_count);
-                },
-                error: function(rs, e) {
-                       alert(rs.responseText);
-                }
-          });
-    })
-</script>
